de2880f13975a644aec8280cccdfb8effcfd8c6e
Paddy Wardle <paddy.wardle@yahoo.com>
2022-06-22 23:06:02 +0100

    api_call.py changes - adding in BeautifulSoup for XML parsing for the properties data

--- main/API_requests/api_call.py
+++ main/API_requests/api_call.py
@@ -7,21 +7,24 @@ import sys
 import os
 from getpass import getpass
 from tqdm import tqdm
+from bs4 import BeautifulSoup
+import lxml
 sys.path.append(os.path.dirname(os.getcwd())+"\\MySQL Database")
 from CreateDB import CreateDB
 
+def pubchem_compound_properties_query(num_records, start_cid=1, write_to_db='yes'):
 
-def pubchem_compound_properties_query(num_records, start_cid=1):
+    if write_to_db.upper() == "YES":
     
+        user = input("Input Database User: ")
+        password = getpass("Input Database Password: ")
+        database = input("Input Database Name: ")
+
+        # writing to MySQL database
+        db_instance = CreateDB(user, password)
+
     # initialise HTTPRequests class
     api_class = HTTPRequests()
-
-    user = input("Input Database User: ")
-    password = getpass("Input Database Password: ")
-    database = input("Input Database Name: ")
-
-    # writing to MySQL database
-    db_instance = CreateDB(user, password)
 
     # api properties
     properties = ['MolecularFormula','MolecularWeight','CanonicalSMILES','IsomericSMILES','InChI','InChIKey','IUPACName','Title','XLogP','ExactMass','MonoisotopicMass','TPSA','Complexity','Charge','HBondDonorCount','HBondAcceptorCount',
@@ -32,26 +35,40 @@ def pubchem_compound_properties_query(num_records, start_cid=1):
     # joining them into a comma-separated string
     properties_string = ','.join(properties)
 
-    # api url with values that can be formatted later <- iterating from starting cid to starting cid + num_records (+1 to get the correct number of records)
+    # api url with values that can be formatted later <- iterating from starting cid to starting cid + num_records
     # because 0 isn't a record in the pubchem database
-    for i in tqdm(range(start_cid, num_records+start_cid+1)):
+    for i in tqdm(range(start_cid, num_records+start_cid)):
 
         try:
 
             # url for properties database
-            properties_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{it}/property/{properties}/JSON'.format(it=i, properties=properties_string)
-
-            # url for images database
-            images_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{it}/PNG?record_type=2d&image_size=small'.format(it=i)
+            properties_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{it}/property/{properties}/XML'.format(it=i, properties=properties_string)
 
             # response for properties database
             properties_response = api_class.get(properties_url)
 
+            # beautiful soup object for XML property data
+            properties_bs = BeautifulSoup(properties_response.content, 'xml')
+
+            # list to store xml tag values
+            properties_data = {}
+
+            # iterating through xml tags
+            for prop in properties:
+                # getting all values for each tag
+                for text in properties_bs.find_all(prop):
+                    b = text
+                    # appending xml tag value to properties_data
+                    properties_data[prop] = [b.get_text()]
+
+            # creating dataframe from properties data (have to reshape as it is a column list)
+            properties_df = pd.DataFrame(properties_data)
+
+            # url for images database
+            images_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{it}/PNG?record_type=2d&image_size=small'.format(it=i)
+
             # response for image database
             images_response = api_class.get(images_url)
-
-            # properties json from response
-            properties_json = properties_response.json()
 
             # image bytes object from response
             image = BytesIO(images_response.content)
@@ -59,17 +76,16 @@ def pubchem_compound_properties_query(num_records, start_cid=1):
             # array with image bytes object
             images_array = np.array([image])
 
-            # properties dataframe with single record
-            properties_df = pd.DataFrame(properties_json['PropertyTable']['Properties'])
-
             # images dataframe with single record
             images_df = pd.DataFrame(images_array, columns=['image'])
 
-            # writing properties dataframe to properties table in database
-            db_instance.write_df('properties', database, properties_df)
+            if write_to_db.upper() == "YES":
 
-            # writing image dataframe to images table in database
-            db_instance.write_df('images', database, images_df)
+                # writing properties dataframe to properties table in database
+                db_instance.write_df('properties', database, properties_df)
+
+                # writing image dataframe to images table in database
+                db_instance.write_df('images', database, images_df)
         
         except AttributeError:
 
@@ -85,5 +101,8 @@ if __name__ == "__main__":
     # number of records user wants
     num_records = int(input("Input the number of records you want: "))
 
+    # write_to_db?
+    write_to_db = input("Would you like to write data to the database?: ")
+
     # calling function
-    pubchem_compound_properties_query(num_records, starting_record)
+    pubchem_compound_properties_query(num_records, starting_record, write_to_db)
