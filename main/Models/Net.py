a5a95d6b8d3bfd2ebc33009efbe7e833458c7e48
Paddy Wardle <paddy.wardle@yahoo.com>
2022-07-03 17:58:18 +0100

    Net.py updated with dropout and batch normalization layers

--- main/Models/Net.py
+++ main/Models/Net.py
@@ -1,28 +1,34 @@
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-
 import torch
-import torchvision
-import torchvision.transforms as transforms
 
 class Net(nn.Module):
     
     def __init__(self):
         super().__init__()
+        self.dropout1 = nn.Dropout(p=0.2)
         self.conv1 = nn.Conv2d(1, 3, 5)
+        self.batchnorm1 = nn.BatchNorm2d(3)
         self.pool = nn.MaxPool2d(2, 2)
+        self.dropout2 = nn.Dropout(p=0.2)
         self.conv2 = nn.Conv2d(3, 5, 5)
-        self.fc1 = nn.Linear(484, 100)
+        self.batchnorm2 = nn.BatchNorm2d(5)
+        self.fc1 = nn.Linear(5*22*22, 100)
         self.fc2 = nn.Linear(100, 50)
         self.fc3 = nn.Linear(50, 1)
         
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = self.dropout1(x)
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        x = F.relu(x)
         x = self.pool(x)
-        x = F.relu(self.conv2(x))
+        x = self.dropout2(x)
+        x = self.conv2(x)
+        x = self.batchnorm2(x)
+        x = F.relu(x)
         x = self.pool(x)
-        x = torch.flatten(x, 1)
+        x = torch.flatten(x, start_dim=1)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
